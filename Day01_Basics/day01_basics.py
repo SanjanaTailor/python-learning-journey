7eb4089a3d466b0065098280553454fd909b78a4
Sanjana <SanjanaTailor@users.noreply.github.com>
2026-09-12 16:11:15 +0530

    Update day01_basics.py

--- Day01_Basics/day01_basics.py
+++ Day01_Basics/day01_basics.py
@@ -1,1 +1,25 @@
 
+# Day 1 - Python Basics
+
+print("Hello, Python!")
+
+# Variables
+name = "Sanjana"
+experience = 14
+target_role = "AI Engineer"
+
+print("Name:", name)
+print("Experience:", experience)
+print("Target Role:", target_role)
+
+# Basic calculations
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# f-string
+print(f"My goal is to become an {target_role}.")
